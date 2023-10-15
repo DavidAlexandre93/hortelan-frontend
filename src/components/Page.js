4ec700549a5f0dfff0947187c37d205d40f43509
David Alexandre Fernandes <davidalexandrefernandes@outlook.com>
2023-10-14 23:05:43 -0300

    fix: structered project

--- src/components/Page.js
+++ src/components/Page.js
@@ -9,7 +9,7 @@ import { Box } from '@mui/material';
 const Page = forwardRef(({ children, title = '', meta, ...other }, ref) => (
   <>
     <Helmet>
-      <title>{`${title} | Versus`}</title>
+      <title>{`${title} | Hortelan`}</title>
       {meta}
     </Helmet>
 
