a587ef534632575eee2b15a75a5a4de3d16d4ce2
David Alexandre Fernandes <davidalexandrefernandes@outlook.com>
2023-10-17 00:20:10 -0300

    fix: structered project

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ThemeProvider from './theme';
 import ScrollToTop from './components/ScrollToTop';
 import { BaseOptionChartStyle } from './components/chart/BaseOptionChart';
 
+
 // ----------------------------------------------------------------------
 
 export default function App() {
