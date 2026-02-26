8fc61e30599f70b47a68bb57eb6c38fa6c64e231
David Alexandre Fernandes <davidalexandrefernandes@Outlook.com>
2026-02-26 02:38:06 -0300

    Redireciona para login após splash screen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import SplashHarvestPro from "./SplashHarvestPro";
+import Router from "./routes";
 
 export default function App() {
   const [showSplash, setShowSplash] = useState(true);
@@ -7,9 +8,6 @@ export default function App() {
   return showSplash ? (
     <SplashHarvestPro onFinish={() => setShowSplash(false)} />
   ) : (
-    <div style={{ padding: 40, fontFamily: "system-ui" }}>
-      <h1>Minha Home real</h1>
-      <p>Conteúdo da HomePage aqui...</p>
-    </div>
+    <Router />
   );
 }
