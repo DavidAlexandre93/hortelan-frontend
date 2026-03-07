681f9edde588b9395f185af8622e286ecadd3221
David Alexandre Fernandes <davidalexandrefernandes@Outlook.com>
2026-03-07 00:11:35 -0300

    refactor: limpar dependências e estabilizar lint/build

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,12 +1,39 @@
-const { FlatCompat } = require('@eslint/eslintrc');
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+const reactHooksPlugin = require('eslint-plugin-react-hooks');
 
 module.exports = [
-  ...compat.config(require('./.eslintrc.cjs')),
   {
     ignores: ['dist/**', 'build/**', 'node_modules/**'],
+    linterOptions: {
+      reportUnusedDisableDirectives: 0,
+    },
+  },
+  {
+    files: ['src/**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+        navigator: 'readonly',
+        PopStateEvent: 'readonly',
+        localStorage: 'readonly',
+        sessionStorage: 'readonly',
+        performance: 'readonly',
+        requestAnimationFrame: 'readonly',
+        cancelAnimationFrame: 'readonly',
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooksPlugin,
+    },
+    rules: {
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_', caughtErrorsIgnorePattern: '^error$', varsIgnorePattern: '^(React|_)' }],
+    },
   },
 ];
